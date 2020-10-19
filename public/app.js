6e6f9a7105c939d860168d83ede6739fdfcfdef5
James Quick <james.q.quick@outlook.com>
2020-10-19 15:51:51 -0500

    Base styling

--- public/app.js
+++ public/app.js
@@ -1,22 +1,10 @@
-const authButtons = document.getElementById('authButtons');
-
-const createAuthButton = (type) => {
-    const loginButton = document.createElement('a');
-    loginButton.href = `/.netlify/functions/${type}`;
-    loginButton.innerText = type.charAt(0).toUpperCase() + type.slice(1);
-    authButtons.append(loginButton);
-};
-
-const createSecretLink = () => {
-    const secretButton = document.createElement('a');
-    secretButton.href = `/admin/secret`;
-    secretButton.innerText = 'View the Secret Stuff!';
-    authButtons.append(secretButton);
-};
-
+const loginBtn = document.getElementById('loginBtn');
+const logoutBtn = document.getElementById('logoutBtn');
+const secretBtn = document.getElementById('secretBtn');
+console.log(loginBtn);
 if (document.cookie.includes('nf_jwt')) {
-    createAuthButton('logout');
-    createSecretLink();
+    logoutBtn.classList.remove('hidden');
+    secretBtn.classList.remove('hidden');
 } else {
-    createAuthButton('login');
+    loginBtn.classList.remove('hidden');
 }
