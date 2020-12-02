25178aa279fbba9252facb0d66ab5882ebbb11a9
James Quick <james.q.quick@outlook.com>
2020-12-02 10:01:21 -0600

    Added buttons for both guest and admin secret pages

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
 const loginBtn = document.getElementById('loginBtn');
 const logoutBtn = document.getElementById('logoutBtn');
-const secretBtn = document.getElementById('secretBtn');
+const secretGuestBtn = document.getElementById('secretGuestBtn');
+const secretAdminBtn = document.getElementById('secretAdminBtn');
 if (document.cookie.includes('nf_jwt')) {
     logoutBtn.classList.remove('hidden');
-    secretBtn.classList.remove('hidden');
+    secretGuestBtn.classList.remove('hidden');
+    secretAdminBtn.classList.remove('hidden');
 } else {
     loginBtn.classList.remove('hidden');
 }
